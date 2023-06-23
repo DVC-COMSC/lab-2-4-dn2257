514441106b39672d62eae53126da4d755c71b4cc
dn2257 <136636075+dn2257@users.noreply.github.com>
2023-06-23 03:41:14 +0000

    Lab 2-4

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    original_string = "Python Programming"
+    sub1 = original_string[0:6]
+    sub2 = original_string[7:19]
+    merge_str = sub2+sub1
+    print(sub2)
+    print(sub1)
+    print(merge_str)
     pass
 
 
